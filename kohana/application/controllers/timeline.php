53b4c55fcc24e561a428a9a487f79bf3d37d5823
Jeff Verkoeyen <jverkoey@gmail.com>
2010-03-10 22:50:29 -0700

    Flesh out the timeline.

--- kohana/application/controllers/timeline.php
+++ kohana/application/controllers/timeline.php
@@ -23,6 +23,262 @@ class Timeline_Controller extends MarkdownTemplate_Controller {
 
     $this->add_css_file('css/timeline.css');
 
+    $events = array(
+      array(
+        'title' => 'UWData.ca',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 1, 15, 2010),
+        'end_date' => mktime(0, 0, 0, 1, 19, 2010),
+        'class' => 'website',
+        'id' => 'uwdata'
+      ),
+      array(
+        'title' => 'Torys',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 2, 1, 2010),
+        'end_date' => mktime(0, 0, 0, 2, 28, 2010),
+        'class' => 'iphone',
+        'id' => 'torys'
+      ),
+      array(
+        'title' => 'Highwind',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 5, 14, 2009),
+        'end_date' => mktime(0, 0, 0, 6, 22, 2009),
+        'class' => 'iphone'
+      ),
+      array(
+        'title' => 'Strips',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 6, 19, 2009),
+        'end_date' => mktime(0, 0, 0, 10, 27, 2009),
+        'class' => 'iphone'
+      ),
+      array(
+        'title' => 'Three20.info',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 11, 24, 2009),
+        'end_date' => time(),
+        'class' => 'website'
+      ),
+      array(
+        'title' => 'Breeze',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 2, 21, 2010),
+        'end_date' => mktime(0, 0, 0, 3, 8, 2010),
+        'class' => 'open-source'
+      ),
+
+
+// Birthdays
+      array(
+        'title' => 'Born',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 8, 18, 1987)
+      ),
+      array(
+        'title' => 'Sister born',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 8, 15, 1992)
+      ),
+
+// Internships
+      array(
+        'title' => 'Sony Media Software',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 5, 1, 2006),
+        'end_date' => mktime(0, 0, 0, 8, 25, 2006),
+        'class' => 'internship'
+      ),
+      array(
+        'title' => 'Sony Creative Software',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 1, 1, 2007),
+        'end_date' => mktime(0, 0, 0, 4, 20, 2007),
+        'class' => 'internship'
+      ),
+      array(
+        'title' => 'Sony Creative Software',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 9, 3, 2007),
+        'end_date' => mktime(0, 0, 0, 12, 14, 2007),
+        'class' => 'internship'
+      ),
+      array(
+        'title' => 'Google',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 5, 5, 2008),
+        'end_date' => mktime(0, 0, 0, 8, 22, 2008),
+        'class' => 'internship'
+      ),
+      array(
+        'title' => 'Facebook',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 1, 5, 2009),
+        'end_date' => mktime(0, 0, 0, 4, 25, 2008),
+        'class' => 'internship'
+      ),
+      array(
+        'title' => 'Facebook',
+        'type' => 'timespan',
+        'date' => mktime(0, 0, 0, 9, 7, 2009),
+        'end_date' => mktime(0, 0, 0, 12, 19, 2008),
+        'class' => 'internship'
+      ),
+
+// University
+      array(
+        'title' => 'University of Waterloo, Term 4A, again',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 1, 4, 2010)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 4A',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 5, 4, 2009)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 3A',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 1, 7, 2008)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 3B',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 1, 2008)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 2B',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 5, 7, 2007)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 1B',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 1, 5, 2006)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 2A',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 4, 2006)
+      ),
+      array(
+        'title' => 'University of Waterloo, Term 1A',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 5, 2005)
+      ),
+
+// High school
+      array(
+        'title' => 'Entered Grade 12',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 6, 2004)
+      ),
+      array(
+        'title' => 'Entered Grade 11',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 1, 2003)
+      ),
+      array(
+        'title' => 'Entered Grade 10',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 2, 2002)
+      ),
+      array(
+        'title' => 'Entered High School, Grade 9',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 3, 2001)
+      ),
+
+// Middle school
+      array(
+        'title' => 'Entered Grade 8',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 4, 2000)
+      ),
+      array(
+        'title' => 'Entered Grade 7',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 6, 1999)
+      ),
+      array(
+        'title' => 'Entered Middle School, Grade 6',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 7, 1998)
+      ),
+
+// Elementary school
+      array(
+        'title' => 'Entered Grade 5',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 1, 1997)
+      ),
+      array(
+        'title' => 'Entered Grade 4',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 2, 1996)
+      ),
+      array(
+        'title' => 'Entered Grade 3',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 4, 1995)
+      ),
+      array(
+        'title' => 'Entered Grade 2',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 5, 1994)
+      ),
+      array(
+        'title' => 'Entered Grade 1',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 6, 1993)
+      ),
+      array(
+        'title' => 'Entered Kindergarten',
+        'type' => 'point',
+        'date' => mktime(0, 0, 0, 9, 7, 1992)
+      )
+    );
+
+    $dates = array();
+    foreach ($events as $event) {
+      $dates []= $event['date'];
+    }
+
+    array_multisort($dates, SORT_ASC, $events);
+
+    $timeline = array();
+
+    foreach ($events as $event) {
+      if ($event['type'] == 'point') {
+        $year = date('Y', $event['date']);
+        if (!isset($timeline[$year])) {
+          $timeline[$year] = array();
+        }
+        $timeline[$year] []= $event;
+      } else if ($event['type'] == 'timespan') {
+        $year = date('Y', $event['date']);
+        $end_date = $event['end_date'];
+        $end_year = date('Y', $end_date);
+        $current_year = intval($year);
+        do {
+          if ($end_year > $current_year) {
+            $event['end_date'] = mktime(0, 0, 0, 12, 31, $current_year);
+          } else {
+            $event['end_date'] = $end_date;
+          }
+          if (!isset($timeline[$current_year])) {
+            $timeline[$current_year] = array();
+          }
+          $timeline[$current_year] []= $event;
+          $current_year++;
+          $event['date'] = mktime(0, 0, 0, 1, 1, $current_year);
+        } while ($current_year <= $end_year);
+      }
+    }
+
+    $content->timeline = $timeline;
+
     $this->render_markdown_template($content);
   }
 
--- kohana/application/views/timeline_content.php
+++ kohana/application/views/timeline_content.php
@@ -4,12 +4,63 @@
 <div id="timeline">
   <div class="header"><h1>Timeline</h1></div>
 <?
+function perc_in_year($date) {
+  return date('z', $date) / 365;
+}
+
   $curr_year = date('Y');
   $curr_month = date('n');
   for ($year = $curr_year; $year >= 1987; --$year) {
-?>
+?>    
+<? if (isset($timeline[$year])) { ?>
   <div class="year">
     <h1><?= $year?></h1>
+    <div class="events">
+    <ul>
+<?    foreach ($timeline[$year] as $event) {
+        if ($event['type'] == 'point') {
+          $date = $event['date'];
+          $perc = perc_in_year($date);
+?>
+        <li style="margin-left: <?= $perc * 100 ?>%">
+          <?= $event['title'] ?>
+          <div class="timestamp"><?= date('l \t\h\e jS \of F', $date)?></div>
+        </li>
+<?      }
+      } ?>
+    </ul>
+    <div class="timespans">
+<?    foreach ($timeline[$year] as $event) {
+        if ($event['type'] == 'timespan') {
+          $date = $event['date'];
+          $perc = perc_in_year($date);
+          $end_date = $event['end_date'];
+          $end_perc = perc_in_year($end_date);
+          $class = isset($event['class']) ? ' '.$event['class'] : '';
+          if ($end_perc - $perc < 0.05) {
+            $class .= ' short_event';
+          }
+?>
+        <div class="timespan<?= $class ?>" style="margin-left: <?= $perc * 100 ?>%;width: <?= ($end_perc - $perc) * 100 ?>%">
+          <div class="content">
+<? if (isset($event['id'])) { ?>
+            <a id="<?= $event['id'] ?>" href="/<?= $event['id'] ?>">
+<? } ?>
+            <?= $event['title'] ?>
+<? if (isset($event['id'])) { ?>
+            </a>
+<? } ?>
+            <div class="info">
+              <div class="timestamp">
+                <?= date('l \t\h\e jS \of F', $date)?> to<br/>
+                <?= date('l \t\h\e jS \of F', $end_date)?></div>
+            </div>
+          </div>
+        </div>
+<?      }
+      } ?>
+    </div>
+    </div>
     <div class="months">
   <? for ($month = 1; $month <= 12; ++$month) { ?>
       <div class="month<?= ($year == $curr_year && $month > $curr_month) ? ' future_month' : ''?>">
@@ -18,7 +69,8 @@
   <? } ?>
       <div class="clearfix"></div>
     </div>
-  </div>
+  </div>  
+<? } ?>
 <? } ?>
 </div> <!-- #timeline -->
 </div> <!-- .fixedwidth -->
