35c988049a10956dc0415d4428b1c4c0aafecd3f
jverkoey <jverkoey@gmail.com>
2011-03-24 17:00:41 -0400

    Add Google Analytics back.

--- kohana/application/views/template.php
+++ kohana/application/views/template.php
@@ -151,12 +151,18 @@
 echo extrahtml::script($js_foot_files, FALSE);
 
 if (IN_PRODUCTION) {
-  echo extrahtml::inlinescript('
-  try {
-  var pageTracker = _gat._getTracker("'.Kohana::config('core.google_analytics_key').'");
-  pageTracker._setDomainName(".jeffverkoeyen.com");
-  pageTracker._trackPageview();
-  } catch(err) {}');
+  echo extrahtml::inlinescript("
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8961075-1']);
+  _gaq.push(['_setDomainName', '.jeffverkoeyen.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+");
 }
 ?>
 
